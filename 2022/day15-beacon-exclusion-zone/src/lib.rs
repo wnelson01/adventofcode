d83a9ebf0c53adc068476495e73dfb550f5844d9
Wade Nelson <nelson.wade.nelson@gmail.com>
2022-12-16 20:22:09 -0500

    Day 15: Beacon Exclusion Zone Part 2

--- 2022/day15-beacon-exclusion-zone/src/lib.rs
+++ 2022/day15-beacon-exclusion-zone/src/lib.rs
@@ -1,4 +1,5 @@
 use regex::Regex;
+use std::collections::HashMap;
 use std::collections::HashSet;
 
 pub fn part_1(input: &str) -> i32 {
@@ -23,36 +24,22 @@ pub fn part_1(input: &str) -> i32 {
             let mdx = md - (row - sy).abs();
             non_beacons.push((sx - mdx, sx + mdx));
         }
-        non_beacons.sort_by(|a, b| a.0.cmp(&b.0));
-        println!("{:?}", non_beacons);
         if non_beacons.len() > 1 {
-            let mut merged_non_beacons = Vec::new();
-            let mut start = non_beacons[0].0;
-            let mut stop = non_beacons[0].1;
-            non_beacons.iter().skip(1).for_each(|beacon| {
-                if beacon.0 > stop {
-                    merged_non_beacons.push((start, stop));
-                    start = beacon.0;
-                    stop = beacon.1;
-                }
-                stop = i32::max(stop, beacon.1);
-            });
-            merged_non_beacons.push((start, stop));
-            non_beacons = merged_non_beacons;
-            println!("merged: {:?}", non_beacons);
+            non_beacons = merge_intervals(&mut non_beacons);
         }
     });
-    println!("{:?}", non_beacons);
-    println!("{:?}", beacons);
+    println!("non_beacons on y = 2000000: {:?}", non_beacons);
+    println!("beacons on y = 2000000: {:?}", beacons);
     (non_beacons[0].0 - non_beacons[0].1).abs()
 }
 
-pub fn merge_intervals(intervals: Vec<(i32, i32)>) -> Vec<(i32, i32)> {
+pub fn merge_intervals(intervals: &mut Vec<(i32, i32)>) -> Vec<(i32, i32)> {
+    intervals.sort_by(|a, b| a.0.cmp(&b.0));
     let mut res = Vec::new();
     let mut start = intervals[0].0;
     let mut stop = intervals[0].1;
     intervals.iter().skip(1).for_each(|interval| {
-        if interval.0 > stop {
+        if interval.0 - 1 >= stop + 1 {
             res.push((start, stop));
             start = interval.0;
             stop = interval.1;
@@ -65,8 +52,7 @@ pub fn merge_intervals(intervals: Vec<(i32, i32)>) -> Vec<(i32, i32)> {
 
 pub fn part_2(input: &str) -> i32 {
     let re = Regex::new(r"(-*[0-9]+)").unwrap();
-    let mut xs = Vec::new();
-    let mut ys = Vec::new();
+    let mut rows: HashMap<i32, Vec<(i32, i32)>> = HashMap::new();
     input.lines().for_each(|line| {
         let coords = re
             .find_iter(line)
@@ -77,13 +63,28 @@ pub fn part_2(input: &str) -> i32 {
         let bx = coords[2];
         let by = coords[3];
         let md = (sx - bx).abs() + (sy - by).abs();
-        xs.push((sx - md, sx + md));
-        ys.push((i32::max(0, sy - md), i32::min(sy + md, 4000000)));
+        (sy - md..=sy + md).for_each(|y| {
+            if 0 <= y && y <= 4000000 {
+                let x = (md - (sy - y).abs()).abs();
+                if 0 <= x && x <= 4000000 {
+                    let mdxy = (sx - x, sx + x);
+                    if rows.contains_key(&y) {
+                        let row = rows.get_mut(&y).unwrap();
+                        row.push(mdxy);
+                        *row = merge_intervals(row);
+                    } else {
+                        rows.insert(y, vec![mdxy]);
+                    }
+                }
+            }
+        });
     });
-    xs = merge_intervals(xs);
-    ys = merge_intervals(ys);
-    println!("{:?}", xs);
-    println!("{:?}", ys);
+    for key in rows.keys() {
+        let xs = rows.get(key).unwrap();
+        if xs.len() > 1 {
+            println!("{:?} {:?}", key, xs);
+        }
+    }
     0
 }
 
